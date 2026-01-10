15656758ec83eedfa1c3b8d2791a81826daca6a5
Karthikeyan1701 <karthikvijay171@gmail.com>
2026-01-10 15:15:22 +0530

    Done with the Axios API instance

--- frontend/src/pages/AdminLogin.jsx
+++ frontend/src/pages/AdminLogin.jsx
@@ -1,3 +1,53 @@
+import { useState } from 'react';
+import api from '../api/axios';
+
 export default function AdminLogin() {
-  return <h1>Admin Login Page</h1>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setMessage('Logging in...');
+
+    try {
+      const res = await api.post('/admin/login', {
+        email,
+        password,
+      });
+
+      setMessage("Login successful!");
+      console.log("Response:", res.data);
+    } catch (error) {
+      setMessage(error.response?.data?.message || 'Login failed');
+    }
+  };
+
+  return (
+    <div>
+      <h2>Admin Login</h2>
+
+      <form onSubmit={handleLogin}>
+        <div>
+          <label>Email</label>
+          <br />
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <label>Password</label>
+          <br />
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </div>
+
+        <button type="submit">Login</button>
+      </form>
+
+      {message && <p>{message}</p>}
+    </div>
+  );
 }
